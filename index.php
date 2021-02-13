88f1629a59eb7de6f4a02f1346eabb0a3e7ac19d
ranetr <78969748+ranetr@users.noreply.github.com>
2021-02-13 18:27:02 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+
+	<input type="hidden" name="token" value="<?=$_SESSION['token']?>"/><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+		<title>RetroLun ~ FLEXİBLEDESİGN</title>
+		
+		<link rel="stylesheet" type="text/css" media="screen" href="https://playnuka.pw/app/tpl/skins/Habwish/styles/index.css?v=1610351435"/>
+		<link rel="stylesheet" type="text/css" media="screen" href="https://playnuka.pw/app/tpl/skins/Habwish/styles/animate.css"/>
+		
+		<style type="text/css">
+			body.index {
+			font-family: Arial;
+			}
+			body.index .clouds {
+				background-image: url(https://playnuka.pw/app/tpl/skins/Habwish/images/index/spook2.png);
+			}
+			#error_red {
+			border: 2px #c93734 solid;
+			border-bottom: 2px #c93734 solid;
+			border-radius: 3px;
+			background-color: #c93734;
+			padding: 10px 10px 10px 10px;
+			margin-bottom: 10px;
+			color: #FFF;
+			text-align: center;
+			}
+						body.index .index-online {
+			background: url(https://playnuka.pw/app/tpl/skins/Habwish/images/index/users-online.png);
+			background-repeat: no-repeat;
+			height: 65px;
+			margin-left: 180px;
+			font-size: 14px;
+			display: flex;
+			align-items: center;
+			}
+
+			
+		</style>
+	</head>
+	<body class="index">
+		<div class="clouds"></div>
+		<div class="index-right"></div>
+		<div class="index-left"></div>
+									<div class="online">
+								<div style="width: 64px; height: 110px; bottom: 96px; position: fixed; margin-left: 188px; float: right; background: url(https://habbo.nl/habbo-imaging/avatarimage?figure=hr-4032-1157-1157.he-4033-71.fa-3865-71.lg-3502-1189.ch-660-1189.wa-3895-74.hd-600-1009.ha-3745-1189-1189&direction=8&head_direction=1&gesture=sml&action=sit,crr=3);" title="Sabre"></div><div style="width: 64px; height: 110px;margin-left: 642px; bottom: 34px; position: fixed; float: right; background: url(https://habbo.nl/habbo-imaging/avatarimage?figure=ch-3332-75-76.hr-3957-61-1345.he-3671-63.sh-3687-1333-1333.hd-3603-26.cc-4041-63.lg-3502-85&direction=7&head_direction=8&gesture=sml);" title="happohemuli"></div>	</div>
+		<div class="login-box" style="margin-top: -175px;">
+			<div class="animated bounceIn">
+				<center>
+					<div class="index-online">
+					<font style="margin-top: -20px;margin-left: 14px;"><b>0</b> Kullanıcı Aktif!</font>	
+					</div>
+										<div class="c-title red"><center>RetroLuna Hoşgeldin!</center></div>
+					
+					<div class="c-main">
+						<form action="index" method="post"> 
+							<div class="input_box user">
+								<input type="text" class="input" placeholder="Kullanıcı Adı" name="log_username">
+							</div>
+							
+							<div class="input_box pass">
+								<input type="password" class="input" placeholder="Şifre" name="log_password">
+								<input type="submit" value="Girişi Tamamla" class="login-btn" name="log_login">
+                                <a href="https://habnet.biz/hotel" style="text-decoration:none;">
+							</div>
+						</form>
+						<a href="http://giraykayali.rf.gd/register.php" style="text-decoration:none;">
+							<div class="register-btn">
+								Kayıt olmadın mı?
+							</div>
+						</a>
+					</div>
+				</center>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
